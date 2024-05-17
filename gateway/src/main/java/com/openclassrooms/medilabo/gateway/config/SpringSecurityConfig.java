69fa0ed18055c736c4ad4878cd30994df9454824
Mikael Heyraud <mikaelhey@hotmail.fr>
2024-05-17 15:47:53 +0200

    MHE: Create Gateway + security

---
+++ gateway/src/main/java/com/openclassrooms/medilabo/gateway/config/SpringSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.openclassrooms.medilabo.gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((req) -> req.requestMatchers("/patient/**").permitAll());
+
+        return http.build();
+    }
+}
