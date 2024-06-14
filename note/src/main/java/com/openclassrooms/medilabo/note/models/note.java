e297329b484f5d43abef86966dbb5fb0913fe746
Mikael Heyraud <mikaelhey@hotmail.fr>
2024-06-14 19:10:52 +0200

    MHE: added MongoDB properties + changed dependencies

--- note/src/main/java/com/openclassrooms/medilabo/note/models/note.java
+++ note/src/main/java/com/openclassrooms/medilabo/note/models/note.java
@@ -1,15 +1,11 @@
 package com.openclassrooms.medilabo.note.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
 @Data
 @Document("notes")
 @AllArgsConstructor
