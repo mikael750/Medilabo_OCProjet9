205fb0c7289ea4999ad7497cad92732edd7f6a3d
Mikael Heyraud <mikaelhey@hotmail.fr>
2024-07-12 15:35:30 +0200

    MHE: add microservice risks

---
+++ risks/src/main/java/com/openclassrooms/projet9/risks/RisksApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.projet9.risks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisksApplication.class, args);
+	}
+
+}
