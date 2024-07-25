90a8df967e67d19d7cbd3e85551c954ce48044a1
Mikael Heyraud <mikaelhey@hotmail.fr>
2024-07-25 18:43:01 +0200

    MHE: clean up

--- risks/src/main/java/com/openclassrooms/medilabo/risks/RisksApplication.java
+++ risks/src/main/java/com/openclassrooms/medilabo/risks/RisksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients("com.openclassrooms.medilabo")
 public class RisksApplication {
 
 	public static void main(String[] args) {
