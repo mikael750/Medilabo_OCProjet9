2bf66ce67c19351186cf3e4f845c474f0ca1afd5
Mikael Heyraud <mikaelhey@hotmail.fr>
2024-09-03 12:09:37 +0200

    MHE: update docker

--- clientui/src/main/java/com/openclassrooms/medilabo/clientui/ClientuiApplication.java
+++ clientui/src/main/java/com/openclassrooms/medilabo/clientui/ClientuiApplication.java
@@ -2,7 +2,10 @@ package com.openclassrooms.medilabo.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients("com.openclassrooms.medilabo")
@@ -10,6 +13,12 @@ public class ClientuiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientuiApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- risks/src/main/java/com/openclassrooms/medilabo/risks/RisksApplication.java
+++ risks/src/main/java/com/openclassrooms/medilabo/risks/RisksApplication.java
@@ -2,7 +2,10 @@ package com.openclassrooms.medilabo.risks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients("com.openclassrooms.medilabo")
@@ -12,4 +15,9 @@ public class RisksApplication {
 		SpringApplication.run(RisksApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
